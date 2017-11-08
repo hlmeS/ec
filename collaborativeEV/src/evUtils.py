37c91cf0c4b4c12d68e160a87a89be4d922e5d82
Holm Smidt <hsmidt@hawaii.edu>
2017-11-07 14:37:02 -1000

    utility fucntion

---
+++ collaborativeEV/src/evUtils.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python2
+
+"""
+
+Title: evUtils.py
+Author: Holm Smidt
+Version: 1.0
+Date: 11-04-2017
+
+Overview:
+Class wrapper for functions that allow help
+with the evolutionary algorithms
+
+
+"""
+
+import numpy as np
+import random
+import math
