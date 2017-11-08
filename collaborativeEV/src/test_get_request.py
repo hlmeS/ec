8f28acaa2a8d5122e8e5df6a5cbacf53325d4e6d
Holm Smidt <hsmidt@hawaii.edu>
2017-11-07 14:36:32 -1000

    Move files to other directory

--- collaborativeEV/src/test_get_request.py
+++
@@ -1,6 +0,0 @@
-import requests
-import urllib
-
-params = urllib.urlencode({'tempset': 53, 'Kp': 7.7, 'valve_lower_limit': 15, 'valve_upper_limit': 255, 'valve_center': 35, 'dcmultiplier':2})
-response = requests.get("http://deep.outtter.space:51880/kahuku_farms/PID_container2/?%s" % params, auth=('user', 'Mat5uda$$'))
-print response
