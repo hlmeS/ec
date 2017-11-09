32ddf6b09716a489ff59878d53867ab9cb6930ca
Holm Smidt <hsmidt@hawaii.edu>
2017-11-08 16:29:05 -1000

    some changes

--- collaborativeEV/src/scheduling.py
+++ collaborativeEV/src/scheduling.py
@@ -10,9 +10,9 @@ import requests, urllib
 
 #PID2?tempset=53&Kp=1.8&Ki=0.1&Kd=0.1&valve_lower_limit=10&valve_upper_limit=120&valve_center=40&windup=20&sampletime=30&dcmultiplier=2
 def execRequest(tempset):
-    params = urllib.urlencode({'tempset': tempset, 'Kp': 1.8, 'Ki': 0.1, 'Kd': 0.1, 'valve_lower_limit': 10, 'valve_upper_limit': 150, 'valve_center': 40, 'windup': 20, 'sampletime':30, 'dcmultiplier':2})
+    params = urllib.urlencode({'tempset': tempset, 'Kp': 1.3, 'Ki': 0.05, 'Kd': 0.4, 'valve_lower_limit': 10, 'valve_upper_limit': 150, 'valve_center': 40, 'windup': 450, 'sampletime':45, 'dcmultiplier':2})
     response = requests.get("http://deep.outtter.space:51880/ilaniwai/PID2?%s" % params, auth=('user', 'Mat5uda$$'))
-    print response
+    return response
 
 """
 Simple algorithm that starts 2 minutes after being called and
@@ -27,7 +27,7 @@ Inputs:
 """
 def schedule(interval, t_low, t_high, t_set):
     #temps = np.linspace(t_low, t_high, t_steps)
-    temps = [t_high, t_low, t_high, t_set, t_low, t_set]
+    temps = [t_high, t_set, t_low, t_set]
     """
     if iters > t_steps and iters <= 2*t_steps:
         currentTemp = np.concatenate((np.random.choice(temps, t_steps, replace=False), np.random.choice(temps, iters-t_steps, replace=False)))
@@ -46,5 +46,5 @@ def schedule(interval, t_low, t_high, t_set):
         execRequest(temps[i])
 
 if __name__ == '__main__':
-    schedule(20, 50, 62, 53)
+    schedule(30, 48, 66, 54)
     execRequest(54)
